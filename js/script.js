e9ec5fd500c8fa91846155334bfb825802b5261a
Nikita <sidmid165@gmail.com>
2022-05-12 18:17:46 +0300

    initialize project

---
+++ js/script.js
@@ -0,0 +1,37 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const tabsContent = document.querySelectorAll('.tabcontent');
+  const tabs = document.querySelectorAll('.tabheader__item');
+  const tabsParent = document.querySelector('.tabheader__items');
+
+  function hideTabContent() {
+    tabsContent.forEach((item) => {
+      item.classList.add('hide');
+      item.classList.remove('show', 'fade');
+    });
+
+    tabs.forEach((element) => {
+      element.classList.remove('tabheader__item_active');
+    });
+  }
+
+  function showTabContent(i = 0) {
+    tabsContent[i].classList.add('show', 'fade');
+    tabsContent[i].classList.remove('hide');
+    tabs[i].classList.add('tabheader__item_active');
+  }
+  hideTabContent();
+  showTabContent();
+
+  tabsParent.addEventListener('click', (event) => {
+    const target = event.target;
+
+    if (target && target.classList.contains('tabheader__item')) {
+      tabs.forEach((element, index) => {
+        if (target == element) {
+          hideTabContent();
+          showTabContent(index);
+        }
+      });
+    }
+  });
+});
